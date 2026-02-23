f385c9fb9c16ebbedd12d8e14bf88d7408009637
BelliaNino <bellianino1996@gmail.com>
2026-02-23 15:50:03 +0100

    add list on page

--- main.js
+++ main.js
@@ -1,12 +1,21 @@
+//variabile per l'API
 const endpoint = 'https://flynn.boolean.careers/exercises/api/random/mail'
 
+const listEl = document.getElementById('listMail')
 
+
+/*tramite "fetch" vado a rihiamare l'API e in seguito vado ad iterare 10 volte grazie al ciclo for()
+per farm restituire,in questo caso, 10 email random*/
+
+listEl.innerHTML = " "
 
 for (let i = 0; i < 10; i++){
 fetch(endpoint)
 .then(res => res.json())
 .then(data => {
     console.log(data.response);
-    
+
+    listEl.innerHTML += `<li>${data.response}</li>`
 })
 }
+
